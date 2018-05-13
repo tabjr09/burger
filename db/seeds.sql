52bf4b9a259003d8dce6c8333aa35a8ad7c8dea2
Thomas Brown <TJ@Thomass-MacBook-Pro.local>
2018-05-13 17:42:54 -0400

    first commit

---
+++ db/seeds.sql
@@ -0,0 +1,8 @@
+use burgers_db;
+
+INSERT INTO burgers (burger_name) VALUES ('Cheeseburger');
+INSERT INTO burgers (burger_name) VALUES ('Turkey Burger');
+INSERT INTO burgers (burger_name, devoured) VALUES ('Bacon Cheeseburger', true);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Grilled Chicken', true);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Double Cheeseburger', true);
+INSERT INTO burgers (burger_name) VALUES ('Black Bean Burger');
